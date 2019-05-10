aa3fdc48fae423e577249d263ab0116012afb598
Jeff Yu <jwy5140@gmail.com>
2019-05-10 11:05:27 -0400

    I tried to make a delete button and something is missing a closing bracket now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+const img = document.getElementById('image');
+const imgName = document.getElementById('name');
+const imgLikes = document.getElementById('likes');
+const imgComments = document.getElementById('comments');
+const imgCard = document.getElementById('image_card');
+const imgCommentForm = document.getElementById('comment_form');
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 2596 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +17,73 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-})
+  fetch(`${imageURL}`)
+  .then(resp => resp.json())
+  .then(json => changeData(json));
+  
+  imgCard.addEventListener('click', (event) => {
+    if (event.target.id === 'like_button'){
+      imgLikes.innerHTML = parseInt(imgLikes.innerHTML) + 1
+      fetch(`${likeURL}`, {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: imageId
+        })
+    })}
+  });
+
+  imgCommentForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let newComment = event.target.comment.value;
+    imgComments.innerHTML += `<li>${newComment} </li>`;
+
+    fetch(`${commentsURL}`,{
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: newComment
+      })
+    })
+  })
+  
+//     imgCommentForm.addEventListener('submit', (event) => {
+//     event.preventDefault()
+
+//     let newComment = event.target.comment.value;
+//     // imgComments.innerHTML += `<li>${newComment} </li>`;
+
+//     fetch(`${commentsURL}`,{
+//       method: 'post',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         image_id: imageId,
+//         content: newComment
+//       })
+//     })
+//     .then(resp => resp.json())
+//     .then(json => )
+//   })
+// })
+
+
+const changeData = (data) => {
+  img.src = data.url;
+  imgName.innerHTML = data.name;
+  imgLikes.innerHTML = data.like_count
+  data.comments.forEach((comment) => {
+    imgComments.innerHTML += `
+    <li>${comment.content} <button id=${comment.id}>Delete Comment</button></li>`
+  })
+}
